2731c9cc773118368c9e663e4d8b189c39a90e97
Alok Saboo <arsaboo@gmx.com>
2023-05-28 17:36:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='beets-jiosaavn',
+    name='beets-metaimport',
     version='0.1',
     description='beets plugin to use JioSaavn for metadata',
     long_description=open('README.md').read(),
     author='Alok Saboo',
     author_email='',
-    url='https://github.com/arsaboo/beets-jiosaavn',
+    url='https://github.com/arsaboo/metaimport',
     license='MIT',
     platforms='ALL',
     packages=['beetsplug'],
     install_requires=[
         'beets>=1.6.0',
-        'MusicAPy @ git+https://github.com/dmdhrumilmistry/MusicAPy',
         'requests',
         'pillow',
     ],
